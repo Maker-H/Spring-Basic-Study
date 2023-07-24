fb58b5e38d8d63589a5f1132cf87412cff8b947f
Heesom <heesom.hs@gmail.com>
2023-07-24 21:54:53 +0900

    [기능] h2 DB 이용하여 JDBC 방식으로 구현
    -JDBCMemberRepository 생성 후 confituration에서 빈으로 생성

--- hello-spring/src/main/java/com/example/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/com/example/hellospring/SpringConfig.java
@@ -1,13 +1,20 @@
 package com.example.hellospring;
 
+import com.example.hellospring.repository.JdbcMemberRepository;
 import com.example.hellospring.repository.MemberRepository;
 import com.example.hellospring.repository.MemoryMemberRepository;
 import com.example.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    @Autowired
+    DataSource dataSource;
 
     @Bean
     public MemberService memberservice() {
@@ -16,6 +23,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
+
+
 }
